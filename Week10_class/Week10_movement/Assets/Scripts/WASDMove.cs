526aa5acbaf441439f3823ca656850f6126865d4
Diego Garcia <radstronomical@gmail.com>
2015-11-02 17:17:51 -0500

    Basic 8-direciton movement

---
+++ Week10_class/Week10_movement/Assets/Scripts/WASDMove.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WASDMove : MonoBehaviour {
+	
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//check if player is pressing w
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += transform.forward * Time.deltaTime * moveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += -transform.forward * Time.deltaTime * moveSpeed;
+		}
+
+		if (Input.GetKey (KeyCode.A))
+		{
+			transform.position += -transform.right * Time.deltaTime * moveSpeed;
+		}
+		if (Input.GetKey (KeyCode.D))
+		{
+			transform.position += transform.right * Time.deltaTime * moveSpeed;
+		}
+
+
+	
+	}
+}
