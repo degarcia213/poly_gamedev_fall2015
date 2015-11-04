f29aeefd0e3393b22012f49d60895794f05a1ef4
Diego Garcia <radstronomical@gmail.com>
2015-11-04 15:20:48 -0500

    rube planning

---
+++ Week_10_Physics_Prep/New Unity Project/Assets/CamCtrl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamCtrl : MonoBehaviour {
+
+	public GameObject[] cams;
+	public int currentCam = 0;
+
+	// Use this for initialization
+	void Start () {
+		ActivateCamera(currentCam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			ActivateCamera(currentCam+1);
+		}
+	}
+
+	public void DeactivateAllCams()
+	{
+		//deactivate all cameras
+		//iterate through the list of each camera
+		for (int i = 0; i < cams.Length; i++)
+		{
+			//deactivate the one at position "i"
+			cams[i].SetActive(false);
+		}
+	}
+
+	public void ActivateCamera(int _camID)
+	{
+		//if the camera id is above the possible cams, set it back to the original.
+		if (_camID >= cams.Length)
+		{
+			_camID = 0;
+		}
+
+		DeactivateAllCams();
+
+		//activate the camera at the _camID position
+		cams[_camID].SetActive(true);
+		//set currentCam to that camera
+		currentCam = _camID;
+	}
+}
---
+++ Week_10_Physics_Prep/New Unity Project/Assets/CamTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTrigger : MonoBehaviour {
+
+	public CamCtrl camCtrl;
+	public GameObject camToActivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		camCtrl.DeactivateAllCams();
+		camToActivate.SetActive(true);
+	}
+}
