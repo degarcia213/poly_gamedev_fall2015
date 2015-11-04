c647213fe2db1285a9787e21ebd09c81810d25be
Diego Garcia <radstronomical@gmail.com>
2015-11-04 17:38:47 -0500

    Camera Controls!!!!!

---
+++ Week_10_Physics_Class/New Unity Project/Assets/Scripts/CamControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamControl : MonoBehaviour {
+
+	public GameObject[] cams;
+	private int currentCam = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		ActivateCam(currentCam);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//detect key press
+		if (Input.GetKeyDown(KeyCode.Space)){
+			//move to the next camera
+			currentCam += 1;
+			if (currentCam == cams.Length)
+			{
+				currentCam = 0;
+			}
+			ActivateCam(currentCam);
+		}
+	
+	}
+
+	public void DeactivateAllCams()
+	{
+		//deactivate all cameras
+		for (int i = 0; i < cams.Length; i++)
+		{
+			cams[i].SetActive(false);
+		}
+	}
+
+	public void ActivateCam(int camIndex){
+
+		DeactivateAllCams();
+
+		//activate camindex cam
+		cams[camIndex].SetActive(true);
+
+	}
+
+
+}
---
+++ Week_10_Physics_Class/New Unity Project/Assets/Scripts/CamTrigger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamTrigger : MonoBehaviour {
+
+	public CamControl camControl;
+	public GameObject camToActivate;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+		//called any time a rigid body enters the collider of my trigger
+	void OnTriggerEnter(Collider other)
+	{
+			
+		//deactivate other cameras
+		camControl.DeactivateAllCams();
+
+		//activate camToActivate
+		camToActivate.SetActive(true);
+
+	}
+}
