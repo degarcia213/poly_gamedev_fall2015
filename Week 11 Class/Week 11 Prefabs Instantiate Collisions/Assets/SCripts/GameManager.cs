b0a45b043f53a00d740b5a2a2828428226aa5eb5
Diego Garcia <radstronomical@gmail.com>
2015-11-23 12:07:25 -0500

    catching up to last week

--- Week 11 Class/Week 11 Prefabs Instantiate Collisions/Assets/SCripts/GameManager.cs
+++ Week 11 Class/Week 11 Prefabs Instantiate Collisions/Assets/SCripts/GameManager.cs
@@ -1,22 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public Player player;
 	public Enemy basicEnemy;
 	public Enemy fierceEnemy;
+
+	HeaderAttribute["SPAWN"]
 	public Transform[] spawnPoints;
+	public Slider healthBar;
+	public Gradient healthColors;
 
 	// Use this for initialization
 	void Start () {
 		// call the SpawnEnemy function once every second afer 1 second.
 		InvokeRepeating("SpawnEnemy", 1f, 1f);
+		healthBar.maxValue = player.maxHealth;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		healthBar.value = player.currentHealth;
+
+		fill1.color = healthColors.Evaluate(currentHealth/maxHealth);
 	
 	}
 
