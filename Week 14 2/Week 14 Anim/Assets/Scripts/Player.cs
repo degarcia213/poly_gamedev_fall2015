bfcc2e1237b9264c5a4efdaa11260677802cc9e6
Diego Garcia <radstronomical@gmail.com>
2015-12-07 13:41:15 -0500

    Animation stuff from class!

---
+++ Week 14 2/Week 14 Anim/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		anim = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			anim.SetBool("isWalking", true);
+		} else {
+
+			anim.SetBool("isWalking", false);
+		}
+
+	}
+}
