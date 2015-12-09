4815f487683cc221feedd717e136217b8b1bbba4
Diego Garcia <radstronomical@gmail.com>
2015-12-09 16:09:36 -0500

    minimap stuff for marcus

--- Week 14 2/Week 14 Anim/Assets/Scripts/Player.cs
+++ Week 14 2/Week 14 Anim/Assets/Scripts/Player.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
+	
+	public Image mapIndicator; 
 
 	Animator anim;
+	Vector2 relativePosition;
 
 	// Use this for initialization
 	void Start () {
@@ -19,11 +23,20 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.W))
 		{
 			anim.SetBool("isWalking", true);
-			transform.position += transform.forward;
+			transform.position += transform.forward * Time.deltaTime * 10;
 		} else {
 
 			anim.SetBool("isWalking", false);
 		}
 
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.RotateAround(transform.position, transform.up, -10);
+		}
+
+		Vector3 mapIndicatorPos = new Vector3(transform.localPosition.x, transform.localPosition.z, 0);
+		mapIndicator.transform.localPosition = mapIndicatorPos * (100f/12.13f);
+		Quaternion myRot = new Quaternion(transform.localRotation.x, transform.localRotation.z, -transform.localRotation.y, transform.localRotation.w);
+		mapIndicator.transform.localRotation = myRot;
 	}
 }
